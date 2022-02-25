82dd209b7af8646d7260d3cc8bc4befd14dd523a
Sean Belverstone <seanbelverstone@gmail.com>
2022-02-25 13:35:54 -0600

    reverted

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import '../css/Navbar.css';
+
+const Navbar = () => {
+	return (
+		<div id="navbarWrapper">
+			<a className="navlinks" href="/">HOME</a>
+			<a className="navlinks" href="/about">ABOUT</a>
+			<a className="navlinks" href="/projects">PROJECTS</a>
+			<a className="navlinks" href="/contact">CONTACT</a>
+		</div>
+	)
+}
+
+export default Navbar;
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import austin from '../assets/austin.jpg'
+
+const Home = () => {
+	return (
+		<div>
+		<img src={austin} alt="Austin, TX"></img>
+		<div>Homepage</div>
+		</div>
+
+	)
+}
+
+export default Home;
