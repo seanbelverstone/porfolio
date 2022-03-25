159bfaa1c082d23e18ebf29d2544ad356aa128f7
Sean Belverstone <seanbelverstone@gmail.com>
2022-03-25 12:29:34 -0500

    using my theme colors on home pages, changing border colors etc.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,24 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { applyActiveClass } from '../utils';
 import './css/Navbar.css';
 
 const Navbar = () => {
+	const [scrollHeight, setScrollHeight] = useState(window.scrollY);
 
 	useEffect(() => {
 		applyActiveClass('navlinks');
 	})
+	useEffect(() => {
+		window.addEventListener('scroll', handleResize, false);
+	}, [])
+
+	const handleResize = () => {
+		setScrollHeight(window.scrollY);
+	}
+
 
 	return (
-		<div id="navbarWrapper">
+		<div id="navbarWrapper" className={scrollHeight > 0 ? 'opaque' : 'transparent'}>
 			<a className="navlinks" href="/">HOME</a>
 			<a className="navlinks" href="/about">ABOUT</a>
 			<a className="navlinks" href="/projects">PROJECTS</a>
