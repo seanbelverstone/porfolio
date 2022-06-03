721fb56c656f7f6c8ae02536f89ed2acaee6410c
Sean Belverstone <seanbelverstone@gmail.com>
2022-06-03 16:29:24 -0500

    hidden the circleGradient if it's a mobile view or if it's on the projects page

--- src/components/CircleGradient.js
+++ src/components/CircleGradient.js
@@ -1,5 +1,12 @@
+import React, {useEffect, useState} from 'react';
 import './css/CircleGradient.css'
 
-export const CircleGradient = () => (
-	<div id="circleGradient" style={{ ...window.innerWidth < 1152 && { display: 'none' } }}/>
-);
+export const CircleGradient = () => {
+	const [pageName, setPageName] = useState('');
+	useEffect(() => {
+		setPageName(document.querySelector('#projectsPage')?.id)
+	}, [])
+	console.log(pageName);
+	return (
+	<div id="circleGradient" style={{ ...(window.innerWidth < 1152 || pageName === 'projectsPage') && { display: 'none' } }}/>
+)};
